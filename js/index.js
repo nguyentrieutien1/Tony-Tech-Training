a368a0dcbb866df2117ad4ce8b90720f69eca056
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-08-16 22:55:06 +0700

    Finished responsive for 4 devices

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-const menu_btn = document.querySelector(".menu");
+const menu_btns = document.querySelectorAll(".menu");
 const header__top = document.querySelector(".header__top");
 const container = document.querySelector(".container");
 const header__nav = document.querySelector(".header__nav--bottom");
 const header__nav_top = document.querySelector(".header__nav--top");
 const close__btn = document.querySelector(".fa-circle-xmark");
 const body = document.querySelector("body");
-
-menu_btn.addEventListener("click", () => {
-  header__top.style.display = "block";
-  container.classList.add("overlay");
-  header__nav.classList.add("hidden__header");
-  header__nav_top.classList.add("hidden__header");
+menu_btns.forEach((menu__btn) => {
+  menu__btn.addEventListener("click", () => {
+    header__top.style.display = "block";
+    container.classList.add("overlay");
+    header__nav.classList.add("hidden__header");
+    header__nav_top.classList.add("hidden__header");
+  });
 });
 close__btn.addEventListener("click", () => {
   header__top.style.display = "none";
