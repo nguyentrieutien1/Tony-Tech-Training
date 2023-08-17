184acabb445f5c767f6a0e85ee07d3573c2e2498
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-08-17 14:43:19 +0700

    handle toggle sidebar function

--- js/index.js
+++ js/index.js
@@ -5,17 +5,13 @@ const header__nav = document.querySelector(".header__nav--bottom");
 const header__nav_top = document.querySelector(".header__nav--top");
 const close__btn = document.querySelector(".fa-circle-xmark");
 const body = document.querySelector("body");
+// START TOGGLE MENU SIDEBAR
 menu_btns.forEach((menu__btn) => {
   menu__btn.addEventListener("click", () => {
     header__top.style.display = "block";
-    container.classList.add("overlay");
-    header__nav.classList.add("hidden__header");
-    header__nav_top.classList.add("hidden__header");
   });
 });
 close__btn.addEventListener("click", () => {
   header__top.style.display = "none";
-  container.classList.remove("overlay");
-  header__nav.classList.remove("hidden__header");
-  header__nav_top.classList.remove("hidden__header");
 });
+// END TOGGLE MENU SIDEBAR
