eb51d81485fb6c294ffac3dcb99b6396cae51823
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-08-17 17:29:28 +0700

    initial js code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+import { products } from "../data/products.js";
 const menu_btns = document.querySelectorAll(".menu");
 const header__top = document.querySelector(".header__top");
 const container = document.querySelector(".container");
@@ -5,13 +6,19 @@ const header__nav = document.querySelector(".header__nav--bottom");
 const header__nav_top = document.querySelector(".header__nav--top");
 const close__btn = document.querySelector(".fa-circle-xmark");
 const body = document.querySelector("body");
+var element = document.querySelector(".sidebar-toggle-label");
 // START TOGGLE MENU SIDEBAR
 menu_btns.forEach((menu__btn) => {
   menu__btn.addEventListener("click", () => {
     header__top.style.display = "block";
+    element.style.opacity = 1;
+    element.style.zIndex = 99999;
   });
 });
 close__btn.addEventListener("click", () => {
   header__top.style.display = "none";
+  element.style.opacity = 0;
+  element.style.zIndex = -1;
 });
+console.log(products);
 // END TOGGLE MENU SIDEBAR
